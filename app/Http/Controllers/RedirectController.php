aca5ca5f76cfab333a08ff2397d702ea18b42172
Brian Mwathi <mwathibrian7@gmail.com>
2016-03-19 11:12:56 +0300

    Corrected Redirect controller

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -17,147 +17,113 @@ use Cas;
 
 class RedirectController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
   public function student($id){
     return view('test')->with('std', $std);
   }
 
-  public function index(){
-    Cas::getCurrentUser();
-    $user = session('cas_user');
-    $client = new \GuzzleHttp\Client();
+  public function index()
+  {
+      Cas::getCurrentUser();
+      $user = session('cas_user');
+      $client = new \GuzzleHttp\Client();
 
-    // $response = $client->get('http://testserver.strathmore.edu:8082/dataservice/student/getStudent/'.$user);
-    $responseStudent = file_get_contents('http://testserver.strathmore.edu:8082/dataservice/student/getStudent/'.$user);
+      // $response = $client->get('http://testserver.strathmore.edu:8082/dataservice/student/getStudent/'.$user);
+      $responseStudent = file_get_contents('http://testserver.strathmore.edu:8082/dataservice/student/getStudent/' . $user);
 
-    $responseStaff = file_get_contents('http://testserver.strathmore.edu:8082/dataservice/staff/getStaff/'.$user);
+      $responseStaff = file_get_contents('http://testserver.strathmore.edu:8082/dataservice/staff/getStaff/' . $user);
 
-    // check to see if the logged in user is a student
-    if ($responseStaff == '' && $responseStudent != ''){
-      $student = json_decode($responseStudent, true);
+      // check to see if the logged in user is a student
+      if ($responseStaff == '' && $responseStudent != '') {
+          $student = json_decode($responseStudent, true);
 
-      // check to see if student has already initiated the clearance process
-      // if student has not requested to be cleared, he/she is taken to the clear request page
-      $std = student::where('studentNo', '=', $user)->first();
+          // check to see if student has already initiated the clearance process
+          // if student has not requested to be cleared, he/she is taken to the clear request page
+          $std = student::where('studentNo', '=', $user)->first();
 
-      // for testing, delete after test
-      // if($student['studentNo'] == '78699'){
-      //   return redirect()->intended('/cafeteria');
-      // }
+          // for testing, delete after test
+          // if($student['studentNo'] == '78699'){
+          //   return redirect()->intended('/cafeteria');
+          // }
 
-      //If the query has a result ...
-      if($std === null){
-          return view('clearance.init')->with('student', $student);
-      }else{
-        $charge=charges::where('students_studentNo','=',  $user)->first();
-        $comment=comment::where('students_studentNo','=', $user)->first();
+          //If the query has a result ...
+          if ($std === null) {
+              return view('clearance.init')->with('student', $student);
+          } else {
+              $charge = charges::where('students_studentNo', '=', $user)->first();
+              $comment = comment::where('students_studentNo', '=', $user)->first();
 
-        return view('clearance.index')->with('std', $std)
-                                      ->with('charge',$charge)
-                                      ->with('comment',$comment);
+              return view('clearance.index')->with('std', $std)
+                  ->with('charge', $charge)
+                  ->with('comment', $comment);
+          }
+      } // If logged in user in a member of staff...
+      elseif ($responseStaff != '' && $responseStudent == '') {
+          $staffMember = json_decode($responseStaff, true);
+          $department_staff_belongs_to = $staffMember['departmentShortName'];
+
+          $check_admin = DB::table('administrators')->where('payroll_number', '=', $staffMember['payroll_number'])->get();
+          if ($check_admin == null) {
+              return "Not Authorized";
+          } else {
+              // redirect the staff member to thier page depending on the school they admin..
+              $is_auth = DB::table('administrators')->where('payroll_number', '=', $staffMember['payroll_number'])->select('administrators.state')->get();
+              if ($is_auth != 'Authorised') {
+                  return "Not Authorized";
+              } else {
+                  if ($department_staff_belongs_to == 'FIT') {
+                      return redirect()->intended('/fit');
+                  } else if ($department_staff_belongs_to == "SLS") {
+                      return redirect()->intended('/sls');
+                  } else if ($department_staff_belongs_to == "SBS") {
+                      return redirect()->intended('/sbs');
+                  } else if ($department_staff_belongs_to == "SFAE") {
+                      return redirect()->intended('/sfae');
+                  } else if ($department_staff_belongs_to == "CTH") {
+                      return redirect()->intended('/cth');
+                  } else if ($department_staff_belongs_to == "SOA") {
+                      return redirect()->intended('/soa');
+                  } else if ($department_staff_belongs_to == "MTI") {
+                      return redirect()->intended('/mti');
+                  } else if ($department_staff_belongs_to == "SHSS") {
+                      return redirect()->intended('/shss');
+                  } else if ($department_staff_belongs_to == "SMC") {
+                      return redirect()->intended('/smc');
+                  } else {
+                      // if the admin does not admin a school. Query by department name
+                      if ($department_staff_belongs_to == "CAFETERIA") {
+                          return redirect()->intended('/cafeteria');
+                      } else if ($department_staff_belongs_to == "LIBRARY") {
+                          return redirect()->intended('/library');
+                      } else if ($department_staff_belongs_to == "FINANCE") {
+                          return redirect()->intended('/finance');
+                      } else if ($department_staff_belongs_to == "FINANCIAL AID") {
+                          return redirect()->intended('/financialAid');
+                      } else if ($department_staff_belongs_to == "GAMES") {
+                          return redirect()->intended('/games');
+                      } else if ($department_staff_belongs_to == "EXTRA CURRICULAR ACTIVITIES") {
+                          return redirect()->intended('/extraCurricularActivities');
+
+                      }
+                  }
+              }
+          }
       }
-    }
-    // If logged in user in a member of staff...
-    elseif($responseStaff != '' && $responseStudent == '') {
-      $staffMember = json_decode($responseStaff, true);
-      $department_staff_belongs_to = $staffMember['departmentShortName'];
-
-      $check_admin = DB::table('administrators')->where('payroll_number', '=', $staffMember['payroll_number'])->get();
-      if($check_admin == null){
-        return "Not Authorized";
-      }else{
-        // redirect the staff member to thier page depending on the school they admin..
-        $is_auth = DB::table('administrators')->where('payroll_number', '=', $staffMember['payroll_number'])->select('administrators.state')->get();
-        if($is_auth != 'Authorised'){
-          return "Not Authorized";
-        }else{
-<<<<<<< HEAD
-          if($department_staff_belongs_to == 'FIT'){
-              return redirect()->intended('/fit');
-          }
-
-          else if($department_staff_belongs_to == "SLS"){
-              return redirect()->intended('/sls');
-          }
-          else if($department_staff_belongs_to == "SBS"){
-              return redirect()->intended('/sbs');
-          }
-          else if($department_staff_belongs_to == "SFAE"){
-              return redirect()->intended('/sfae');
-          }
-          else if($department_staff_belongs_to == "CTH"){
-              return redirect()->intended('/cth');
-          }
-          else if($department_staff_belongs_to == "SOA"){
-              return redirect()->intended('/soa');
-          }
-          else if($department_staff_belongs_to == "MTI"){
-              return redirect()->intended('/mti');
-          }
-          else if($department_staff_belongs_to == "SHSS"){
-              return redirect()->intended('/shss');
-          }
-          else if($department_staff_belongs_to == "SMC"){
-              return redirect()->intended('/smc');
-          }else{
-            // if the admin does not admin a school. Query by department name
-            if($department_staff_belongs_to == "Cafeteria"){
-                return redirect()->intended('/cafeteria');
-            }
-            else if($department_staff_belongs_to == "Library"){
-                return redirect()->intended('/library');
-            }
-            else if($department_staff_belongs_to == "Finance"){
-                return redirect()->intended('/finance');
-            }
-            else if($department_staff_belongs_to == "Financial Aid"){
-                return redirect()->intended('/financialAid');
-            }
-            else if($department_staff_belongs_to == "Games"){
-                return redirect()->intended('/games');
-            }
-            else if($department_staff_belongs_to == "Extra-curricular"){
-                return redirect()->intended('/extraCurricularActivities');
-            }
-=======
-          // if the admin does not admin a school. Query by department name
-          if($department_staff_belongs_to == "CAFETERIA"){
-              return redirect()->intended('/cafeteria');
-          }
-          else if($department_staff_belongs_to == "LIBRARY"){
-              return redirect()->intended('/library');
-          }
-          else if($department_staff_belongs_to == "FINANCE"){
-              return redirect()->intended('/finance');
-          }
-          else if($department_staff_belongs_to == "FINANCIAL AID"){
-              return redirect()->intended('/financialAid');
-          }
-          else if($department_staff_belongs_to == "GAMES"){
-              return redirect()->intended('/games');
-          }
-          else if($department_staff_belongs_to == "EXTRA CURRICULAR ACTIVITIES"){
-              return redirect()->intended('/extraCurricularActivities');
->>>>>>> d756e3cd4113a03e44650e6f043c7d409d0e8b5f
-          }
-        }
-      }
-    }
   }
 
 
-  public function destroy(){
-   Cas::logout();
-   return redirect()->intended('/');
- }
+    public function destroy(){
+      Cas::logout();
+      return redirect()->intended('/');
+    }
     public function getDepartment(){
-        $responseStaff = file_get_contents('http://testserver.strathmore.edu:8082/dataservice/staff/getStaff/'.$user);
-        $staffMember = json_decode($responseStaff, true);
-        return $staffMember['departmentShortName'];
+      $responseStaff = file_get_contents('http://testserver.strathmore.edu:8082/dataservice/staff/getStaff/1');
+      $staffMember = json_decode($responseStaff, true);
+      return $staffMember['departmentShortName'];
 
     }
-}
+  }
